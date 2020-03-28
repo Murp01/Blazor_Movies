a53b89fa458cb44367f30031dffe72477cc1e57a
Murp01 <pmurphy35@hotmail.com>
2020-03-28 13:03:26 +0000

    Added a movie entity that is called in a paragraph.  Each initialized movie title and date is shown.  If number is odd in one colour even another.

---
+++ Blazor_Movies/Entities/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Movies.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ Blazor_Movies/Helpers/StringUtilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Movies.Helpers
+{
+    public class StringUtilities
+    {
+        public static string CustomToUpper(string value) => value.ToUpper();
+
+    }
+}
